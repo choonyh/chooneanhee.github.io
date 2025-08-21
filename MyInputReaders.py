31b745fac39c17d5211b2733d68c691994fbfa7b
choonyh <choonyeanhee@gmail.com>
2025-08-21 08:57:39 +0800

    update MyInputReaders.py

--- MyInputReaders.py
+++ MyInputReaders.py
@@ -1,12 +1,4 @@
 def read_string(prompt_msg):
-	"""
-	Purpose:
-	Reads a string from user and removes leading and trailing whitespace.
-	Parameters:
-	prompt_msg (str): The message displayed to the user when prompting for
-	input.
-	Returns:
-	str: The cleaned string entered by the user.
-	"""
-    
+
 	return input(prompt_msg).strip()
+
